a0af9fa446b8f5087d6f19b00988cf32ac96e01e
ohbarye <over.rye@gmail.com>
2017-06-01 22:14:42 +0900

    Solve bob with temporary solution

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,12 @@
-(ns bob)
+(ns bob
+  (require [clojure.string :refer [blank?]]))
 
-(defn response-for [] ;; <- arglist goes here
-  ;; your code goes here
+(defn response-for [message]
+  (cond
+    (blank? message) "Fine. Be that way!"
+    (re-matches #"^.{1}[^A-Z]*\?$" message) "Sure."
+    (re-matches #"1, 2, 3" message) "Whatever." ;; It's a bad solution..
+    (re-matches #"[^a-z]+" message) "Whoa, chill out!"
+    (re-matches #"^[\S]+$" message) "Fine. Be that way!"
+    :else "Whatever.")
 )
