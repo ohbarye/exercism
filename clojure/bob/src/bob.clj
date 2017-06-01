08f3b5ead8d502a1e63b6d18dd350bc52025f407
ohbarye <over.rye@gmail.com>
2017-06-01 22:34:47 +0900

    Solve bob version 2

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,12 +1,16 @@
 (ns bob
-  (require [clojure.string :refer [blank?]]))
+  (require [clojure.string :refer [blank? upper-case lower-case ends-with?]]))
+
+(defn yell? [message]
+  (and (= message (upper-case message)) (not= message (lower-case message))))
+
+(defn question? [message]
+  (ends-with? message "?"))
 
 (defn response-for [message]
   (cond
     (blank? message) "Fine. Be that way!"
-    (re-matches #"^.{1}[^A-Z]*\?$" message) "Sure."
-    (re-matches #"1, 2, 3" message) "Whatever." ;; It's a bad solution..
-    (re-matches #"[^a-z]+" message) "Whoa, chill out!"
-    (re-matches #"^[\S]+$" message) "Fine. Be that way!"
+    (yell? message) "Whoa, chill out!"
+    (question? message) "Sure."
     :else "Whatever.")
 )
