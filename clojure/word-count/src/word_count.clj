bd6a94825d2fc95274e2bba36590b908f2dc73fd
ohbarye <over.rye@gmail.com>
2017-06-06 00:29:43 +0900

    Solve word-count again

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -4,8 +4,5 @@
 (defn split-by-delmiter [arg]
   (map lower-case (split arg #"[^A-Za-z0-9]+")))
 
-(defn count-by-word [m v]
-  (assoc m v (+ 1 (if (m v) (m v) 0))))
-
 (defn word-count [arg]
-  (reduce count-by-word {} (split-by-delmiter arg)))
+  (frequencies (split-by-delmiter arg)))
