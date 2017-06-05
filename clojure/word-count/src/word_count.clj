a6fc90536cc661a8ad83bf241d002e4607fa9594
ohbarye <over.rye@gmail.com>
2017-06-05 23:31:16 +0900

    Solve word count

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,5 +1,11 @@
-(ns word-count)
+(ns word-count
+  (require [clojure.string :refer [lower-case split]]))
 
-(defn word-count [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn split-by-delmiter [arg]
+  (map lower-case (split arg #"[^A-Za-z0-9]+")))
+
+(defn count-by-word [m v]
+  (assoc m v (+ 1 (if (m v) (m v) 0))))
+
+(defn word-count [arg]
+  (reduce count-by-word {} (split-by-delmiter arg)))
