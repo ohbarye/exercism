4c1f255cbdb51f5d9c51456447cb777ddd4341fc
ohbarye <over.rye@gmail.com>
2017-06-10 00:07:02 +0900

    Solve anagram

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,5 +1,22 @@
-(ns anagram)
+(ns anagram
+  (require [clojure.string :refer [split join lower-case]]))
 
-(defn anagrams-for [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn normalize [word]
+  (join
+    (sort
+      (split
+        (lower-case word)
+        #""))))
+
+(defn same? [x y]
+  (= (lower-case x)
+     (lower-case y)))
+
+(defn anagram? [x y]
+  (and (= (normalize x) (normalize y))
+       (not (same? x y))))
+
+(defn anagrams-for [word candidates]
+  (filter
+    #(anagram? word %)
+    candidates))
